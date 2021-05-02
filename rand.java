fedb7a2580f1764e062203b2b33f267b702ed47c
salcedochanza <72934619+salcedochanza@users.noreply.github.com>
2021-05-02 14:02:37 +0200

    Add files via upload

---
+++ rand.java
@@ -0,0 +1,13 @@
+public class rand {
+	 
+    public static void main(String[] args) {
+ 
+        double rand = Math.random();
+        if (rand >= 0.5){
+            System.out.println("1"); 
+        } else {
+            System.out.println("0");
+        }
+    }
+ 
+}
